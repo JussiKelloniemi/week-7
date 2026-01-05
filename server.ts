cb4edab5944e1c271db0b92d6fdf165c3be15f7e
JussiKelloniemi <jussi.kelloniemi@student.lut.fi>
2026-01-05 23:14:08 +0200

    Task 1 fixes

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const app: Express = express()
-const port: number = parseInt(process.env.PORT as string) || 3000
+const port = 3000
 
 const mongoDB: string = "mongodb://127.0.0.1:27017/testdb"
 mongoose.connect(mongoDB)
